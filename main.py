588da37e252cff77f1122194357a03fb012328c0
Kristine <71175273+UXKK@users.noreply.github.com>
2022-02-26 13:09:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+print('Hello,\n"World!"')
+
+print('hello,')
+print("'world!''")
+
+print("\\")
+
+print("my name")
+print()
+print("is KK")
+
+print("""My name is
+
+KK""")
+
+print("""
+_____
+|   |
+-----
+      
+      """)
+
+print("my", "name", "is", "KK")
+
+print("my", "name", "is", "KK", sep="_")
+
+print("my name is", end="")
+print("KK")
+
+print("Hello","My name is", sep=".", end=" ")
+print("KK", end=".")
+
